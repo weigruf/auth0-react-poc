d174b8b0982564718c8d179ee145bcdbaee7922e
Panagiotis Andreou <866272+weigruf@users.noreply.github.com>
2022-09-14 10:21:56 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import { BrowserRouter as Router } from "react-router-dom";
+//import Auth0Provider from "@auth0/auth0-react";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
+
+import "./index.css";
+
+ReactDOM.render(
+  <Router>
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
+  </Router>,
+  document.getElementById("root")
+);
